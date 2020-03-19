ff3c098c42deabedf92c63f1c1ddf59899949864
WangZhengWei1016 <wzw487923438@gmail.com>
2020-03-19 21:43:45 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        title: 'HelloWorld'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
